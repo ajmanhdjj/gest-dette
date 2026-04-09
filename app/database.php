9518a69af647a5abf86993fd200a7226e6078a46
AJMAN Hadjiboudine <a.hadjiboudine2016@gmail.com>
2026-04-09 14:54:36 +0000

    feat: update database configuration to connect to Aiven MySQL instance via SSL

--- app/database.php
+++ app/database.php
@@ -1,5 +1,30 @@
 <?php
-$servername = 'localhost';
-$username = 'root';
-$dbpassword = 'root';
-$dbname = 'db_gest_dette';
+
+$servername = "mysql-675a44e-ajmanhdj.h.aivencloud.com";
+$username = "avnadmin";
+$password = "AVNS_nFUFjrCHKteVKGgRVRn";
+$dbname = "defaultdb";
+$port = 17559;
+
+$conn = mysqli_init();
+
+mysqli_ssl_set($conn, NULL, NULL, "../database/ca.pem", NULL, NULL);
+
+mysqli_real_connect(
+    $conn,
+    $servername,
+    $username,
+    $password,
+    $dbname,
+    $port,
+    NULL,
+    MYSQLI_CLIENT_SSL
+);
+
+if (!$conn) {
+    die("Connexion échouée : " . mysqli_connect_error());
+}
+
+echo "Connexion réussie";
+
+?>
