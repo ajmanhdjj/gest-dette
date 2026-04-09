baad3820a935220d5981887f9f34b6f64dc2f33b
AJMAN Hadjiboudine <a.hadjiboudine2016@gmail.com>
2026-04-09 14:54:20 +0000

    refactor: update database configuration to use local development credentials

--- app/database.php
+++ app/database.php
@@ -1,30 +1,5 @@
 <?php
-
-$servername = "mysql-675a44e-ajmanhdj.h.aivencloud.com";
-$username = "avnadmin";
-$password = "AVNS_nFUFjrCHKteVKGgRVRn";
-$dbname = "defaultdb";
-$port = 17559;
-
-$conn = mysqli_init();
-
-mysqli_ssl_set($conn, NULL, NULL, "../database/ca.pem", NULL, NULL);
-
-mysqli_real_connect(
-    $conn,
-    $servername,
-    $username,
-    $password,
-    $dbname,
-    $port,
-    NULL,
-    MYSQLI_CLIENT_SSL
-);
-
-if (!$conn) {
-    die("Connexion échouée : " . mysqli_connect_error());
-}
-
-echo "Connexion réussie";
-
-?>
+$servername = 'localhost';
+$username = 'root';
+$dbpassword = 'root';
+$dbname = 'db_gest_dette';
